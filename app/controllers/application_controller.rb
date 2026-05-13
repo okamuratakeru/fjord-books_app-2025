e2c001cc16969c2672a235706be966f00622ebeb
okamuratakeru <takeokamura11@gmail.com>
2026-05-13 11:01:03 +0900

    feat/多言語対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = I18n.available_locales.map(&:to_s).include?(params[:locale]) ? params[:locale] : I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
