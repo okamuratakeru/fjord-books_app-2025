b7a5c231d874f88f828be540857185f66f035689
okamuratakeru <takeokamura11@gmail.com>
2026-05-19 13:58:52 +0900

    /feat

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
     accounts_path
   end
 
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
+
   protected
 
   def configure_account_update_params
