a1042ff5e3630cb7e41ded4594f26c540b67d96e
okamuratakeru <takeokamura11@gmail.com>
2026-05-19 14:45:42 +0900

    動作確認漏れていた項目を修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  def create
+    super do |resource|
+      if resource.persisted?
+        sign_out resource
+        redirect_to new_user_session_path and return
+      end
+    end
+  end
+
   protected
 
   def after_update_path_for(resource)
