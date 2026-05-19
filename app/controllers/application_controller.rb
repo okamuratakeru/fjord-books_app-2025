c1cd4e1619f167cfbe0e76873f9ea0db87312e1b
okamuratakeru <takeokamura11@gmail.com>
2026-05-19 14:36:13 +0900

    fix/要件もれの対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   before_action :authenticate_user!
   before_action :configure_account_update_params, if: :devise_controller?
+  before_action :configure_sign_up_params, if: :devise_controller?
 
   def after_sign_in_path_for(_resource)
     accounts_path
@@ -18,5 +19,9 @@ class ApplicationController < ActionController::Base
 
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: %i[postal_code address bio])
+  end
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[postal_code address bio])
   end
 end
