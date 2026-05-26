09a4a55d267410ac935a42f32d1176c2973df01f
okamuratakeru <takeokamura11@gmail.com>
2026-05-26 13:24:30 +0900

    レビュー修正/orderをつける

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,7 +2,7 @@
 
 class AccountsController < ApplicationController
   def index
-    @users = User.page(params[:page]).per(20)
+    @users = User.order(:id).page(params[:page]).per(20)
   end
 
   def show
