6c81eee9de2a3d0f1bb833cf1f3bb2e1dad76185
okamuratakeru <takeokamura11@gmail.com>
2026-05-30 14:09:14 +0900

    レビュー修正/model側がわに置く

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,7 +1,4 @@
 # frozen_string_literal: true
 
 module ReportsHelper
-  def owner?(resource)
-    resource.user == current_user
-  end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -3,4 +3,8 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+
+  def owned_by?(user)
+    self.user == user
+  end
 end
