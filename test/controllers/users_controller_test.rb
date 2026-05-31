1e4163511ad3310102ffd29af3152b0cbacc8d36
okamuratakeru <takeokamura11@gmail.com>
2026-05-31 12:03:55 +0900

    test/初期テストエラーを修正

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,13 +3,19 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    sign_in users(:one)
+  end
+
   test 'should get index' do
-    get users_index_url
+    get users_url
     assert_response :success
   end
 
   test 'should get show' do
-    get users_show_url
+    get user_url(users(:one))
     assert_response :success
   end
 end
