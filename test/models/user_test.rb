7b43d82f1cb906322c4b893728cd87729a081855
okamuratakeru <takeokamura11@gmail.com>
2026-06-09 08:16:40 +0900

    レビュー修正/Fixturesはあくまでテストには関係がないデフォルト値のみセットするようにする

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,12 +5,12 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   describe '#name_or_email' do
     it '名前があれば名前を返す' do
-      user = users(:with_name)
+      user = User.new(name: 'テストユーザー', email: 'with_name@example.com', password: 'password')
       assert_equal 'テストユーザー', user.name_or_email
     end
 
     it '名前がなければメールアドレスを返す' do
-      user = users(:without_name)
+      user = User.new(email: 'without_name@example.com', password: 'password')
       assert_equal 'without_name@example.com', user.name_or_email
     end
   end
