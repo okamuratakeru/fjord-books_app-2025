d8b0c5b04d6f81a9c7bbee9cd5bff2522a274d31
okamuratakeru <takeokamura11@gmail.com>
2026-06-10 09:29:57 +0900

    レビュー修正/-s で終わるようにする

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,16 +3,16 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
-  has_many :report_links_as_source, class_name: 'ReportLink',
-                                    foreign_key: :source_report_id,
-                                    dependent: :destroy,
-                                    inverse_of: :source_report
-  has_many :report_links_as_target, class_name: 'ReportLink',
-                                    foreign_key: :target_report_id,
-                                    dependent: :destroy,
-                                    inverse_of: :target_report
-  has_many :mentioned_reports,  through: :report_links_as_source, source: :target_report
-  has_many :mentioning_reports, through: :report_links_as_target, source: :source_report
+  has_many :source_report_links, class_name: 'ReportLink',
+                                 foreign_key: :source_report_id,
+                                 dependent: :destroy,
+                                 inverse_of: :source_report
+  has_many :target_report_links, class_name: 'ReportLink',
+                                 foreign_key: :target_report_id,
+                                 dependent: :destroy,
+                                 inverse_of: :target_report
+  has_many :mentioned_reports,  through: :source_report_links, source: :target_report
+  has_many :mentioning_reports, through: :target_report_links, source: :source_report
 
   validates :title, presence: true
   validates :content, presence: true
@@ -35,14 +35,14 @@ class Report < ApplicationRecord
     new_target_ids = content.scan(REPORT_URL_PATTERN).flatten.map(&:to_i).uniq
     new_target_ids.delete(id)
 
-    current_target_ids = report_links_as_source.pluck(:target_report_id)
+    current_target_ids = source_report_links.pluck(:target_report_id)
 
     ids_to_add    = new_target_ids - current_target_ids
     ids_to_remove = current_target_ids - new_target_ids
 
-    report_links_as_source.where(target_report_id: ids_to_remove).destroy_all
+    source_report_links.where(target_report_id: ids_to_remove).destroy_all
     ids_to_add.each do |target_id|
-      report_links_as_source.find_or_create_by!(target_report_id: target_id)
+      source_report_links.find_or_create_by!(target_report_id: target_id)
     rescue ActiveRecord::RecordNotFound
       nil
     end
